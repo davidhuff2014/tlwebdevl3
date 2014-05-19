01fccdaf18c2ba9fb9b07d5c98d8ea8c568b604e
davidhuff2014 <David.Huff@computer-critters.com>
2014-05-19 18:19:14 -0400

    working the lessons, what about this LF/CRLF stuff?

--- main.rb
+++ main.rb
@@ -5,5 +5,29 @@ require 'sinatra/reloader'
 
 set :sessions, true
 get '/home'  do
-  'Welcome Home to Ruby, again and again, Dave!'
+  'Welcome Home to Ruby, again!'
+end
+
+get '/form' do
+  erb :form
+end
+
+post '/myaction' do
+  params['username']
+end
+
+get '/inline' do
+  'Hi frmo the action'
+end
+
+get '/template' do
+  erb :mytemplate
+end
+
+get '/nested_template' do
+  erb :'users/profile'
+end
+
+get '/nothere' do
+  redirect '/inline'
 end
