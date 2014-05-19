ac6a30546c0898a282e781167ec34cce56dec31c
davidhuff2014 <David.Huff@computer-critters.com>
2014-05-19 12:59:17 -0400

    getting reloader working in winblows

--- main.rb
+++ main.rb
@@ -1,8 +1,9 @@
+# encoding: UTF-8
 require 'rubygems'
 require 'sinatra'
+require 'sinatra/reloader'
 
 set :sessions, true
-
-
-
-
+get '/home'  do
+  'Welcome Home to Ruby, again and again, Dave!'
+end
