8caf297912aa836efdd26efb4df1d12acafcaf02
davidhuff2014 <David.Huff@computer-critters.com>
2014-05-19 22:37:58 -0400

    updated game design

---
+++ game_design.rb
@@ -0,0 +1,13 @@
+# looks like the demo is using 4 routes
+# new_game # gets name
+# bet # makes wager
+# game # is game play display cards and total
+# start over in the nav bar
+# click to view dealers next cards, I don't like
+# most of the activity is on the game
+# displaying cards is nice
+# displaying running total of amount won or lost
+# play again?
+# game_over displays two different things one if
+# you go bust and another if you quit before
+# display bet outcomes and amounts
