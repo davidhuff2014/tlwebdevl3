70264ee0699b058361a8b3fe3f335835ce288f05
davidhuff2014 <David.Huff@computer-critters.com>
2014-05-20 12:58:50 -0400

    after lunch snapshot

--- main.rb
+++ main.rb
@@ -4,30 +4,42 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 set :sessions, true
-get '/home'  do
-  'Welcome Home to Ruby, again!'
+
+get '/' do
+  redirect '/new_game'
 end
 
-get '/form' do
-  erb :form
+get '/new_game' do
+  erb :'users/new_game'
 end
 
-post '/myaction' do
-  params['username']
+# get '/home'  do
+#   'Welcome Home to Ruby, again!'
+# end
+
+# get '/form' do
+#   erb :form
+# end
+
+post '/game' do
+  # params['username']
+  session[:name] = params[:username]
+  text =  session[:name]
+  puts text
 end
 
-get '/inline' do
-  'Hi from the action!'
-end
+# get '/inline' do
+#   'Hi from the action!'
+# end
 
-get '/template' do
-  erb :mytemplate
-end
+# get '/template' do
+#   erb :mytemplate
+# end
 
-get '/nested_template' do
-  erb :'users/profile'
-end
+# get '/nested_template' do
+#   erb :'users/profile'
+# end
 
-get '/nothere' do
-  redirect '/inline'
-end
+# get '/nothere' do
+#   redirect '/inline'
+# end
