d7c2b0542e7b00bf4751ae0741952b8f31d62f84
davidhuff2014 <David.Huff@computer-critters.com>
2014-05-22 13:17:14 -0400

    after lunch snapshot

--- main.rb
+++ main.rb
@@ -5,12 +5,47 @@ require 'sinatra/reloader'
 
 set :sessions, true
 
+helpers do
+  def calc_total(cards)
+    arr = cards.map { |e| e[1] }
+
+    total = 0
+    arr.each do |value|
+      if value == 'A'
+        total += 11
+      elsif value.to_i == 0
+        total += 10
+      else
+        total += value.to_i
+      end
+    end
+
+    arr.select { |e| e == 'A' }.count.times do
+      total -= 10 if total > 21
+    end
+    session[:total] = total
+  end
+end
+
 get '/' do
   if session[:player_name]
-    session[:player_name] = ''  # take out after testing
-    redirect '/new_player'
-    # redirect '/bet'           # add back after testign
+    session[:player_name] = ''    # take out after testing
+
+    # redirect '/bet'             # add back after testign
+    session[:player_kitty] = 500  # take out after testing
+    session[:player_score] = 0
+    session[:dealer_score] = 0
+    session[:player_cards] = []
+    session[:dealer_cards] = []
+    session[:player_hits] = false
+    redirect '/new_player'        # change redirect after testing
   else
+    session[:player_score] = 0
+    session[:dealer_score] = 0
+    session[:player_kitty] = 500
+    session[:player_cards] = []
+    session[:dealer_cards] = []
+    session[:player_hits] = false
     redirect '/new_player'
   end
 end
@@ -25,17 +60,30 @@ post '/new_player' do
 end
 
 get '/game' do
-  suits = %w(H D C S)
-  values = %w(2 3 4 5 6 7 8 9 10 J Q K A)
-  session[:deck] = suits.product(values).shuffle!
-  session[:player_cards] = []
-  session[:dealer_cards] = []
-  session[:player_cards] << session[:deck].pop
-  session[:dealer_cards] << session[:deck].pop
-  session[:player_cards] << session[:deck].pop
-  session[:dealer_cards] << session[:deck].pop
-  session[:player_score] = 0
-  session[:dealer_score] = 0
+  if session[:player_score] == 0
+    suits = %w(H D C S)
+    values = %w(2 3 4 5 6 7 8 9 10 J Q K A)
+    session[:deck] = suits.product(values).shuffle!
+    # session[:player_cards] = []
+    # session[:dealer_cards] = []
+    session[:player_cards] << session[:deck].pop
+    session[:dealer_cards] << session[:deck].pop
+    session[:player_cards] << session[:deck].pop
+    session[:dealer_cards] << session[:deck].pop
+
+    calc_total(session[:player_cards])
+    session[:player_score] = session[:total]
+    calc_total(session[:dealer_cards])
+    session[:dealer_score] = session[:total]
+
+    session[:player_hits] = false
+  end
+
+  if session[:player_hits] == true
+    session[:player_cards] << session[:deck].pop
+    calc_total(session[:player_cards])
+    session[:player_score] = session[:total]
+  end
 
   erb :game
 end
@@ -50,40 +98,20 @@ post '/bet' do
   redirect '/game'
 end
 
-# get '/game/player/hit' do
-#   if session[:player_score] == 21
-#     session[:end_message] = 'You have won!'
-#     erb :game_over
-#   end
-#   if session[:player_kitty] == 0
-#     session[:end_message] = 'You are out of money'
-#     erb :game_over
-#   end
-# end
-
 post '/game/player/hit' do
-  redirect '/game_over'
+  session[:player_hits] = true
+  redirect '/game'
+  # session[:player_score] == 21  ? session[:end_message] = 'You have won!'         : ''
+  # session[:player_kitty] == 0   ? session[:end_message] = 'You are out of money'  : ''
+  # session[:player_kitty] >= 500 ? session[:end_message] = 'Quitting so soon?'     : ''
+
+  # redirect '/game_over'
 end
 
 get '/game_over' do
-  if session[:player_score] == 21
-    session[:end_message] = 'You have won!'
-    erb :game_over
-  end
-  if session[:player_kitty] == 0
-    session[:end_message] = 'You are out of money'
-    erb :game_over
-  end
   erb :game_over
 end
 
-# post '/game_over' do
-#   if session[:player_score] == 21
-#     session[:end_message] = 'You have won!'
-#     # erb :game_over
-#   end
-#   if session[:player_kitty] == 0
-#     session[:end_message] = 'You are out of money'
-#     # erb :game_over
-#   end
-# end
+post '/game/player/stay' do
+  redirect '/game'
+end
